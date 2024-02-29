077fd2522fc276ac510dc34c8ec6cc0b09cc4185
Erick <erickfrias8006@gmail.com>
2024-02-29 11:57:51 -0300

    Meu primeiro comit

---
+++ Projeto-RPG/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Projeto_RPG;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Projeto-RPG/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Projeto_RPG;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
