d45fd2cc54510f55b14e0f76436d40d6997429a1
Erick <erickfrias8006@gmail.com>
2024-03-05 09:09:15 -0300

    Segundo Comit

---
+++ Projeto-RPG/obj/Debug/net8.0-android/android/src/crc64edb747066777913f/MainActivity.java
@@ -0,0 +1,48 @@
+package crc64edb747066777913f;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("Projeto_RPG.MainActivity, Projeto-RPG", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Projeto_RPG.MainActivity, Projeto-RPG", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Projeto_RPG.MainActivity, Projeto-RPG", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
